07f39791ef3d4ccdf5601e4db4f786943bd10b1f
Skile <imleejm@gmail.com>
2021-05-22 04:33:10 +0900

    feat: setup service worker

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
+import initializeFirebase from '../utils';
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js').then(
+          async (registration) => {
+            const token  = await initializeFirebase(registration);
+            console.log({token});
+          },
+          err => console.error(err),
+        )
+      });
+    }
+  }, []);
+
   return <Component {...pageProps} />
 }
 export default MyApp
