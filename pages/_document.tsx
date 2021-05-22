b15c3891f9337ed82ba7ea807e8d2a71228b8710
Skile <imleejm@gmail.com>
2021-05-22 13:19:26 +0900

    fix: remove Not Found link

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,39 +28,7 @@ class MyDocument extends Document {
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#000000" />
 
-          <link
-            rel="apple-touch-icon"
-            href="/static/icons/touch-icon-iphone.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="152x152"
-            href="/static/icons/touch-icon-ipad.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="167x167"
-            href="/static/icons/touch-icon-ipad-retina.png"
-          />
-
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/icons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/icons/favicon-16x16.png"
-          />
           <link rel="manifest" href="/static/manifest.json" />
-          <link
-            rel="mask-icon"
-            href="/static/icons/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
           <link rel="shortcut icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
