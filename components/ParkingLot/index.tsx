6b4c21eca73ac777fb234807b7e2d094c6af3a4a
beomjungil <gil@beomjun.kr>
2021-05-22 19:09:07 +0900

    feat: Add ParkingLotList Backdrop

--- components/ParkingLot/index.tsx
+++ components/ParkingLot/index.tsx
@@ -1,7 +1,7 @@
 import { ParkingLotData } from "../../utils/types";
 
 const ParkingLot: React.FC<ParkingLotData> = ({ ...data }) => {
-  return <div>{data.name}</div>;
+  return <div className="h-8">{data.name}</div>;
 }
 
 export default ParkingLot;
