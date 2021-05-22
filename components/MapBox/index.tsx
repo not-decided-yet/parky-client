1f89b9506aaa3a75e2948f75dd4f326bb1d2cecd
beomjungil <gil@beomjun.kr>
2021-05-22 19:09:07 +0900

    feat: Add ParkingLotList Backdrop

--- components/MapBox/index.tsx
+++ components/MapBox/index.tsx
@@ -8,10 +8,11 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_TOKEN!;
 const MAP_CONTAINER_ID = "map-container";
 
 interface MapProps {
+  className?: string;
   parkingLots: ParkingLotData[];
 }
 
-const MapBox: React.FC<MapProps> = ({ parkingLots }) => {
+const MapBox: React.FC<MapProps> = ({ parkingLots, className }) => {
   const [map, setMap] = useState<mapboxgl.Map>();
 
   useEffect(() => {
@@ -40,7 +41,11 @@ const MapBox: React.FC<MapProps> = ({ parkingLots }) => {
     );
   }, [map, parkingLots]);
 
-  return <div id={MAP_CONTAINER_ID} className="w-screen h-screen"></div>;
+  return (
+    <div className={`w-screen h-screen z-0 ${className}`}>
+      <div id={MAP_CONTAINER_ID} className="w-full h-full" />
+    </div>
+  );
 };
 
 export default MapBox;
--- components/ParkingLotList/index.tsx
+++ components/ParkingLotList/index.tsx
@@ -1,16 +1,46 @@
 import { ParkingLotData } from "../../utils/types";
 import ParkingLot from "../ParkingLot";
+import Sheet from "react-modal-sheet";
 
 export interface ParkingLotListProps {
+  className?: string;
   items: ParkingLotData[];
 }
 
-export default function ParkingLotList({ items }: ParkingLotListProps) {
+const ParkingLotList: React.FC<ParkingLotListProps> = ({
+  items,
+  className,
+}) => {
   return (
-    <div>
-      {items.map((data, index) => (
-        <ParkingLot key={index} {...data} />
-      ))}
-    </div>
+    <Sheet
+      isOpen={true}
+      onClose={() => {}}
+      snapPoints={[600, 100]}
+      initialSnap={1}
+      className={`parking-lot-list ${className}`}
+    >
+      <Sheet.Container>
+        {/* <Sheet.Header /> */}
+        <Sheet.Content>
+          {items.map((data, index) => (
+            <ParkingLot key={index} {...data} />
+          ))}
+        </Sheet.Content>
+      </Sheet.Container>
+
+      <Sheet.Backdrop />
+    </Sheet>
+    // <BottomSheet
+    //   header={items.map((data, index) => (
+    //     <ParkingLot key={index} {...data} />
+    //   ))}
+    //   snapPoints={({ maxHeight }) => [maxHeight / 4, maxHeight * 0.8]}
+    //   open
+    //   className={classNames("bg-white", className)}
+    // >
+    //   <div />
+    // </BottomSheet>
   );
-}
+};
+
+export default ParkingLotList;
