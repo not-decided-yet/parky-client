7f1e649266ea6356d1c8a903ce9d7e255e116b65
beomjungil <gil@beomjun.kr>
2021-05-22 18:13:28 +0900

    chore(dummy): Add dummy Parking lots data

---
+++ dummies/parkingLots.ts
@@ -0,0 +1,16 @@
+import { ParkingLotData } from "../utils/types";
+
+const dummyParkingLots: ParkingLotData[] = [
+  {
+    _id: "",
+    name: "Union Square Parking",
+    longitude: 122.41095490000001,
+    latitude: 37.783797,
+    is_free: false,
+    lots: "",
+    priority: "",
+    created_at: new Date(),
+  },
+];
+
+export default dummyParkingLots;
