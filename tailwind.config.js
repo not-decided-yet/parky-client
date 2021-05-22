e135fa373b38226c47bca9f7272299cdfea65391
beomjungil <gil@beomjun.kr>
2021-05-22 18:27:22 +0900

    style(theme): Add brand colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4560FF",
+        accent: "#FFBF0D",
+        white: "#FFFFFF",
+        black: "#232326"
+       },
+    },
   },
   variants: {
     extend: {},
