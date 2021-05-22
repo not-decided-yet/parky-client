e892a9895296b91e67b8712910e340c00f2347ee
beomjungil <gil@beomjun.kr>
2021-05-22 23:24:00 +0900

    chore: Add manifest.json, Favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,8 +16,11 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap" rel="stylesheet"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Manrope:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
           <meta name="application-name" content="Parky" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta
@@ -30,25 +33,21 @@ class MyDocument extends Document {
           <meta name="mobile-web-app-capable" content="yes" />
           <meta
             name="msapplication-config"
-            content="/static/icons/browserconfig.xml"
+            content="/icons/browserconfig.xml"
           />
           <meta name="msapplication-TileColor" content="#4560FF" />
           <meta name="msapplication-tap-highlight" content="no" />
           <meta name="theme-color" content="#4560FF" />
 
-          <link rel="manifest" href="/static/manifest.json" />
-          <link rel="shortcut icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
 
           <meta name="twitter:card" content="summary" />
           <meta name="twitter:url" content="https://parky.ml" />
           <meta name="twitter:title" content="Parky" />
-          <meta
-            name="twitter:description"
-            content="It's a Parky day!"
-          />
+          <meta name="twitter:description" content="It's a Parky day!" />
           <meta
             name="twitter:image"
-            content="https://parky.ml/static/icons/android-chrome-192x192.png"
+            content="https://parky.ml/icons/android-chrome-192x192.png"
           />
           <meta property="og:type" content="website" />
           <meta property="og:title" content="Parky" />
@@ -57,43 +56,108 @@ class MyDocument extends Document {
           <meta property="og:url" content="https://parky.ml" />
           <meta
             property="og:image"
-            content="https://parky.ml/static/icons/apple-touch-icon.png"
+            content="https://parky.ml/icons/apple-touch-icon.png"
           />
 
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_2048.png"
+            href="/icons/apple_splash_2048.png"
             sizes="2048x2732"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1668.png"
+            href="/icons/apple_splash_1668.png"
             sizes="1668x2224"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1536.png"
+            href="/icons/apple_splash_1536.png"
             sizes="1536x2048"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1125.png"
+            href="/icons/apple_splash_1125.png"
             sizes="1125x2436"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_1242.png"
+            href="/icons/apple_splash_1242.png"
             sizes="1242x2208"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_750.png"
+            href="/icons/apple_splash_750.png"
             sizes="750x1334"
           />
           <link
             rel="apple-touch-startup-image"
-            href="/static/images/apple_splash_640.png"
+            href="/icons/apple_splash_640.png"
             sizes="640x1136"
+          />
+
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="57x57"
+            href="/icons/apple-touch-icon-57x57.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="114x114"
+            href="/icons/apple-touch-icon-114x114.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="72x72"
+            href="/icons/apple-touch-icon-72x72.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="144x144"
+            href="/icons/apple-touch-icon-144x144.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="60x60"
+            href="/icons/apple-touch-icon-60x60.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="120x120"
+            href="/icons/apple-touch-icon-120x120.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="76x76"
+            href="/icons/apple-touch-icon-76x76.png"
+          />
+          <link
+            rel="apple-touch-icon-precomposed"
+            sizes="152x152"
+            href="/icons/apple-touch-icon-152x152.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/icons/favicon-196x196.png"
+            sizes="196x196"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/icons/favicon-96x96.png"
+            sizes="96x96"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/icons/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/icons/favicon-16x16.png"
+            sizes="16x16"
           />
         </Head>
         <body>
