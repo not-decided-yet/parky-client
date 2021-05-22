2db7f33f58e5ac5902b9cd3bca1d2b52e4e7ec94
Skile <imleejm@gmail.com>
2021-05-23 02:25:21 +0900

    feat: set marker resources

--- components/MapBox/index.tsx
+++ components/MapBox/index.tsx
@@ -7,6 +7,15 @@ mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_GL_TOKEN!;
 
 const MAP_CONTAINER_ID = "map-container";
 
+const MARKER_RESOURCES = {
+  marker_available: {
+    "text-size": 14,
+    "text-offset": [0, -0.2],
+  },
+  marker_focus: { "text-size": 20, "text-offset": [0, -0.3] },
+  marker_unavailable: { "text-size": 0, "text-offset": [0, 0] },
+};
+
 interface MapProps {
   className?: string;
   parkingLots: ParkingLotData[];
@@ -28,14 +37,11 @@ const createMap = (defaultLocation: Coordinate) => {
     })
   );
 
-  [
-    { name: "marker_available" },
-    { name: "marker_focus", content: [20, 20, 75, 65] },
-    { name: "marker_unavailable" },
-  ].forEach(({ name: imageName, ...options }) => {
+  Object.keys(MARKER_RESOURCES).forEach((imageName) => {
     map.loadImage(
       `/img/${imageName}.png`,
-      (err, image) => image && map.addImage(imageName, image, {pixelRatio: 2})
+      (err, image) =>
+        image && map.addImage(imageName, image, { pixelRatio: 2.5 })
     );
   });
 
@@ -56,50 +62,61 @@ const MapBox: React.FC<MapProps> = ({
 
   useEffect(() => {
     if (!map) return;
-    parkingLots.forEach(({ longitude, latitude }, index) => {
-      const coord: Coordinate = [longitude, latitude];
-      // new mapboxgl.Marker().setLngLat(coord).addTo(map);
+    const features = parkingLots.map(({ longitude, latitude }, index) => {
+      const markerType = [
+        "marker_focus",
+        "marker_unavailable",
+        "marker_available",
+      ][index % 3];  // TODO: set using focus index & ...
 
-      map.on("load", () => {
-        const SOURCE_ID = `test-source-name-${index}`;
-        map.addSource(SOURCE_ID, {
-          type: "geojson",
-          data: {
-            type: "FeatureCollection",
-            features: [
-              {
-                type: "Feature",
-                geometry: {
-                  type: "Point",
-                  coordinates: coord,
-                },
-                properties: {
-                  "image-name": "marker_focus",
-                  name: "7",
-                },
-              },
-            ],
-          },
-        });
+      return {
+        type: "Feature",
+        geometry: {
+          type: "Point",
+          coordinates: [longitude, latitude],
+        },
+        properties: {
+          name: "3",  // TODO: set available
+          "image-name": markerType,
+          ...MARKER_RESOURCES[markerType],
+        },
+      };
+    });
 
-        map.addLayer({
-          id: SOURCE_ID,
-          type: "symbol",
-          source: SOURCE_ID,
-          layout: {
-            "text-field": ["get", "name"],
-            "icon-text-fit": "none",
-            "icon-image": ["get", "image-name"],
-            "icon-allow-overlap": true,
-            "text-allow-overlap": true,
-          },
-        });
+    // new mapboxgl.Marker().setLngLat(coord).addTo(map);
+    map.on("load", () => {
+      const SOURCE_ID = `test-source-name`;
+
+      map.addSource(SOURCE_ID, {
+        type: "geojson",
+        data: {
+          type: "FeatureCollection",
+          features: features, // @ts-ignore
+        },
+      });
+
+      map.addLayer({
+        id: SOURCE_ID,
+        type: "symbol",
+        source: SOURCE_ID,
+        layout: {
+          "text-field": ["get", "name"],
+          "icon-text-fit": "none",
+          "icon-image": ["get", "image-name"],
+          "icon-allow-overlap": true,
+          "text-allow-overlap": true,
+          "text-size": ["get", "text-size"],
+          "text-offset": ["get", "text-offset"],
+        },
       });
     });
   }, [map, parkingLots]);
 
   return (
-    <div className={`w-screen h-screen z-0 ${className}`}>
+    <div
+      className={`w-screen h-screen z-0 ${className}`}
+      style={{ color: "#ffffff" }}
+    >
       <div id={MAP_CONTAINER_ID} className="w-full h-full" />
     </div>
   );
--- dummies/parkingLots.ts
+++ dummies/parkingLots.ts
@@ -14,8 +14,8 @@ const dummyParkingLots: ParkingLotData[] = [
   {
     _id: "",
     name: "Union Square Parking2",
-    longitude: -122.41095490000001,
-    latitude: 37.783797,
+    longitude: -122.4100,
+    latitude: 37.783792,
     is_free: true,
     lots: "",
     priority: "",
@@ -24,8 +24,8 @@ const dummyParkingLots: ParkingLotData[] = [
   {
     _id: "",
     name: "Union Square Parking3",
-    longitude: -122.41095490000001,
-    latitude: 37.783797,
+    longitude: -122.410953,
+    latitude: 37.7835,
     is_free: true,
     lots: "",
     priority: "",
