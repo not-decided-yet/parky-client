2e127334a62c28b5cc91e54695e875990df4d9b0
Skile <imleejm@gmail.com>
2021-05-23 11:27:52 +0900

    feat: 테스트용 푸쉬 토큰 추출 엔드포인트 추가

---
+++ pages/test_push.tsx
@@ -0,0 +1,32 @@
+import { useRouter } from "next/dist/client/router";
+import React, { useEffect, useState } from "react";
+import { useAppContext } from "../context/state";
+import initializeNotification from "../utils/fcm";
+
+export default function TestPush() {
+  const [token, setToken] = useState<string>("");
+  useEffect(() => {
+    if (!("serviceWorker" in navigator)) return;
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then(initializeNotification)
+      .then(setToken);
+  });
+
+  return (
+    <div className="w-full h-full p-6">
+      {token ? (
+        <button
+          className="bg-primary text-white p-4 w-full rounded-xl"
+          onClick={() => {
+            navigator.clipboard.writeText(token);
+          }}
+        >
+          Copy my Push Token
+        </button>
+      ) : (
+        "Loading..."
+      )}
+    </div>
+  );
+}
